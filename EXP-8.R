32386bed65c50b8b77fa7e3ee9b84fdc81cd5d4e
kishore yadav <126038832+kishore71437@users.noreply.github.com>
2024-07-23 10:29:26 +0530

    Create EXP-8.R

---
+++ EXP-8.R
@@ -0,0 +1,11 @@
+# Define the x and y values
+x <- c(4, 1, 5, 7, 10, 2, 50, 25, 90, 36)
+y <- c(12, 5, 13, 19, 31, 7, 153, 72, 275, 110)
+
+# Create a scatter plot
+plot(x, y, 
+     xlab = "Number of Mobile Phones Sold", 
+     ylab = "Money", 
+     main = "Scatter Plot of Mobile Phones Sold vs Money",
+     pch = 19,           # Type of point
+     col = "blue")       # Color of points
