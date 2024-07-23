b2381ea04a799b12abffa24cffc39b1f23ddad0f
kishore yadav <126038832+kishore71437@users.noreply.github.com>
2024-07-23 10:07:47 +0530

    Create EXP-2 c .R

---
+++ EXP-2 c .R
@@ -0,0 +1,3 @@
+ages <- c(13, 15, 16, 16, 19, 20, 20, 21, 22, 22, 25, 25, 25, 25, 30, 33, 33, 35, 35, 35, 35, 36, 40, 45, 46, 52, 70)
+midrange_age <- (min(ages) + max(ages)) / 2
+cat("Midrange of the data:", midrange_age, "\n")
